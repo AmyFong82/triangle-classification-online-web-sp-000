57c64cce6b049c5094df79111a11d33101bdee81
Amy Fong <amyhfong@gmail.com>
2020-02-13 17:49:43 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -25,7 +25,7 @@ class Triangle
       true
     when side_1 == 0 || side_2 == 0 || side_3 == 0
       true
-    when side_1 < 0 || side_2 < 0 or side_3 < 0
+    when side_1 < 0 || side_2 < 0 || side_3 < 0
       true
     else
       FALSE
