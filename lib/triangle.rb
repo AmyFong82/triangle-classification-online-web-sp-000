79f3432a42b6ca2d4bf4ec957e3a365a2cd25112
Amy Fong <amyhfong@gmail.com>
2020-02-13 17:56:16 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -20,7 +20,7 @@ class Triangle
   end
   
   def invalid_triangle(side_1, side_2, side_3)
-    if side_1 + side_2 < side_3
+    if side_1 + side_2 < side_3 || side_2 + side_3 < side_1 || side_1 + side_3 < side_2 
       true
     elsif side_1 == 0 || side_2 == 0 || side_3 == 0
       true
